33ab81614720bae4bff5baeee54c150e7ca09b8f
Félix Wilhelmy <fwilhelmy@hotmail.com>
2026-02-01 04:11:02 -0500

    first cleanup of the circuits

--- src/circuits/AaronCircuit.py
+++ src/circuits/AaronCircuit.py
@@ -1,52 +1,57 @@
-from __future__ import annotations
+def aaron_circuit_2pairs():
+    qc = QuantumCircuit(4, 3)
+    qc.cx(1, 0)
+    qc.cx(2, 3)
+    qc.measure(0, 0)
+    qc.measure(3, 1)
+    with qc.if_test((qc.clbits[0], 1)):
+        qc.x(1)
+    with qc.if_test((qc.clbits[1], 1)):
+        qc.x(2)
+    qc.reset(0)
+    with qc.if_test((qc.clbits[0], 1)):
+        qc.x(0)
+    with qc.if_test((qc.clbits[1], 1)):
+        qc.x(0)
+    qc.measure(0, 2)
+    return qc, 2
 
-from typing import Any, Dict
-
-from qiskit import QuantumCircuit
-
-from circuits.BaseCircuit import BaseCircuit
-
-
-class AaronCircuit(BaseCircuit):
-    """Two-pair recurrence (BBPSSW/DEJMPS-style) distillation circuit."""
-
-    min_bell_pairs = 2
-    max_bell_pairs = 2
-
-    def get_num_bell_pairs(self, edge: Dict[str, Any]) -> int:
-        return 2
-
-    def get_flag_qubit(self, edge: Dict[str, Any]) -> int:
-        return 2
-
-    def build_circuit(self, edge: Dict[str, Any]) -> tuple[QuantumCircuit, int, int]:
-        num_bell_pairs = self.get_num_bell_pairs(edge)
-        flag_bit = self.get_flag_qubit(edge)
-        if num_bell_pairs != 2:
-            raise ValueError("AaronCircuit is fixed to two Bell pairs.")
-
-        qc = QuantumCircuit(4, 3)
-
-        qc.cx(1, 0)
-        qc.cx(2, 3)
-        
-        qc.measure(0, 0)
-        qc.measure(3, 1)
-        
-        # Corrections
-        with qc.if_test((qc.clbits[0], 1)):
-            qc.x(1)
-        with qc.if_test((qc.clbits[1], 1)):
+def aaron_circuit_3pairs_xx():
+    qc = QuantumCircuit(6, 5)
+    for i in range(2):
+        qc.cx(2, i)
+        qc.cx(3, 5-i)
+    for i in range(2):
+        qc.measure(i, i)
+        qc.measure(5-i, 2+i)
+    for i in range(2):
+        with qc.if_test((qc.clbits[i], 1)):
             qc.x(2)
-        
-        # Flag calculation: c[2] = c[0] XOR c[1]
-        # If both 0 or both 1: c[2] = 0 (keep)
-        # If different: c[2] = 1 (discard)
-        qc.reset(0)  # Reuse qubit 0 for flag
-        with qc.if_test((qc.clbits[0], 1)):
-            qc.x(0)
-        with qc.if_test((qc.clbits[1], 1)):
-            qc.x(0)
-        qc.measure(0, flag_bit)
+        with qc.if_test((qc.clbits[2+i], 1)):
+            qc.x(3)
+    qc.reset(1)
+    for i in range(4):
+        with qc.if_test((qc.clbits[i], 1)):
+            qc.x(1)
+    qc.measure(1, 4)
+    return qc, 3
 
-        return qc, num_bell_pairs, flag_bit
+def aaron_circuit_4pairs():
+    qc = QuantumCircuit(8, 7)
+    for i in range(3):
+        qc.cx(3, i)
+        qc.cx(4, 7-i)
+    for i in range(3):
+        qc.measure(i, i)
+        qc.measure(7-i, 3+i)
+    for i in range(3):
+        with qc.if_test((qc.clbits[i], 1)):
+            qc.x(3)
+        with qc.if_test((qc.clbits[3+i], 1)):
+            qc.x(4)
+    qc.reset(1)
+    for i in range(6):
+        with qc.if_test((qc.clbits[i], 1)):
+            qc.x(1)
+    qc.measure(1, 6)
+    return qc, 4
--- src/circuits/ShaneCircuit.py
+++ src/circuits/ShaneCircuit.py
@@ -70,91 +70,11 @@ def create_z():
     qc.name = "Z_distillation"
     return qc
 
-def shane_block_xx_x_4():
-    """Example distillation circuit template for 2 Bell pairs."""
-    num_bell_pairs = 4
-    qr = QuantumRegister(num_bell_pairs*2, 'q')  # 4 qubits for 2 Bell pairs
-    cr = ClassicalRegister(num_bell_pairs*3, 'c')  # Classical bits for measurements + flag
-    qc = QuantumCircuit(qr, cr)
-
-    qz = create_z()
-    qx = create_x()
-    ##ORDERING IS IMPORTANT HEREqc,[0,2,3,1],range(3)
-    qc.compose(qx, qubits=[0,2,3,1], clbits=range(3),inplace=True)
-    qc.compose(qx, qubits=[6,4,5,7], clbits=range(3,6),inplace=True)
-    qc.compose(qz, qubits=[2,3,4,5], clbits=range(6,9),inplace=True)
-
-    false1 = expr.bit_or(expr.lift(qc.clbits[2]),expr.lift(qc.clbits[5]))
-    qc.store(qc.clbits[9],false1) 
-    false_condition = expr.bit_or(expr.lift(qc.clbits[8]),expr.lift(qc.clbits[9]))
-    qc.store(qc.clbits[10],false_condition)
-
-    return qc, 4
-
-def aaron_circuit_2pairs():
-    qc = QuantumCircuit(4, 3)
-    qc.cx(1, 0)
-    qc.cx(2, 3)
-    qc.measure(0, 0)
-    qc.measure(3, 1)
-    with qc.if_test((qc.clbits[0], 1)):
-        qc.x(1)
-    with qc.if_test((qc.clbits[1], 1)):
-        qc.x(2)
-    qc.reset(0)
-    with qc.if_test((qc.clbits[0], 1)):
-        qc.x(0)
-    with qc.if_test((qc.clbits[1], 1)):
-        qc.x(0)
-    qc.measure(0, 2)
-    return qc, 2
-
-def aaron_circuit_3pairs_xx():
-    qc = QuantumCircuit(6, 5)
-    for i in range(2):
-        qc.cx(2, i)
-        qc.cx(3, 5-i)
-    for i in range(2):
-        qc.measure(i, i)
-        qc.measure(5-i, 2+i)
-    for i in range(2):
-        with qc.if_test((qc.clbits[i], 1)):
-            qc.x(2)
-        with qc.if_test((qc.clbits[2+i], 1)):
-            qc.x(3)
-    qc.reset(1)
-    for i in range(4):
-        with qc.if_test((qc.clbits[i], 1)):
-            qc.x(1)
-    qc.measure(1, 4)
-    return qc, 3
-
-def aaron_circuit_4pairs():
-    qc = QuantumCircuit(8, 7)
-    for i in range(3):
-        qc.cx(3, i)
-        qc.cx(4, 7-i)
-    for i in range(3):
-        qc.measure(i, i)
-        qc.measure(7-i, 3+i)
-    for i in range(3):
-        with qc.if_test((qc.clbits[i], 1)):
-            qc.x(3)
-        with qc.if_test((qc.clbits[3+i], 1)):
-            qc.x(4)
-    qc.reset(1)
-    for i in range(6):
-        with qc.if_test((qc.clbits[i], 1)):
-            qc.x(1)
-    qc.measure(1, 6)
-    return qc, 4
-
-
 def shane_distillation_circuit_1():
     "Basically, the identity"
     qc = QuantumCircuit(2, 1)
 
-def shane_distillation_circuit_2(t):
+def shane_distillation_circuit_2(t): # THIS ONE
     """Example distillation circuit template for 2 Bell pairs."""
     """Minimum case"""
     "t = z or x"
