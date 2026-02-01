3888f2a65155873ab8335d51041e56bd43417bf1
Lukas Rapp <rappl@mit.edu>
2026-02-01 01:27:58 -0500

    added circuits aaron.

--- src/circuits/CircuitList.py
+++ src/circuits/CircuitList.py
@@ -17,7 +17,7 @@ def measure_z_errors():
 
     qc.store(cr[2], expr.bit_xor(expr.lift(cr[0]), expr.lift(cr[1])))
 
-    return qc
+    return qc, 1
 
 def measure_x_errors():
     """
@@ -37,9 +37,9 @@ def measure_x_errors():
 
     qc.store(cr[2], expr.bit_xor(expr.lift(cr[0]), expr.lift(cr[1])))
 
-    return qc
+    return qc, 1
 
-def Shane_block_layout_xx__x_4():
+def shane_block_xx_x_4():
     """Example distillation circuit template for 2 Bell pairs."""
     num_bell_pairs = 4
     qr = QuantumRegister(num_bell_pairs*2, 'q')  # 4 qubits for 2 Bell pairs
@@ -88,4 +88,63 @@ def Shane_block_layout_xx__x_4():
     false_condition = expr.bit_or(expr.lift(qc.clbits[8]),expr.lift(qc.clbits[9]))
     qc.store(qc.clbits[10],false_condition)
 
-    return qc
+    return qc, 4
+
+def aaron_circuit_2pairs():
+    qc = QuantumCircuit(4, 3)
+    qc.cx(1, 0)
+    qc.cx(2, 3)
+    qc.measure(0, 0)
+    qc.measure(3, 1)
+    with qc.if_test((qc.clbits[0], 1)):
+        qc.x(1)
+    with qc.if_test((qc.clbits[1], 1)):
+        qc.x(2)
+    qc.reset(0)
+    with qc.if_test((qc.clbits[0], 1)):
+        qc.x(0)
+    with qc.if_test((qc.clbits[1], 1)):
+        qc.x(0)
+    qc.measure(0, 2)
+    return qc, 2
+
+def aaron_circuit_3pairs_xx():
+    qc = QuantumCircuit(6, 5)
+    for i in range(2):
+        qc.cx(2, i)
+        qc.cx(3, 5-i)
+    for i in range(2):
+        qc.measure(i, i)
+        qc.measure(5-i, 2+i)
+    for i in range(2):
+        with qc.if_test((qc.clbits[i], 1)):
+            qc.x(2)
+        with qc.if_test((qc.clbits[2+i], 1)):
+            qc.x(3)
+    qc.reset(1)
+    for i in range(4):
+        with qc.if_test((qc.clbits[i], 1)):
+            qc.x(1)
+    qc.measure(1, 4)
+    return qc, 3
+
+def aaron_circuit_4pairs():
+    qc = QuantumCircuit(8, 7)
+    for i in range(3):
+        qc.cx(3, i)
+        qc.cx(4, 7-i)
+    for i in range(3):
+        qc.measure(i, i)
+        qc.measure(7-i, 3+i)
+    for i in range(3):
+        with qc.if_test((qc.clbits[i], 1)):
+            qc.x(3)
+        with qc.if_test((qc.clbits[3+i], 1)):
+            qc.x(4)
+    qc.reset(1)
+    for i in range(6):
+        with qc.if_test((qc.clbits[i], 1)):
+            qc.x(1)
+    qc.measure(1, 6)
+    return qc, 4
+
