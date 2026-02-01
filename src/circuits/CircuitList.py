a29b9ccf6b58d49aa63e9181ab1389b437405cdf
Centiganda851 <shanetendoj@gmail.com>
2026-02-01 06:19:17 +0000

    Block4 in CircuitList

--- src/circuits/CircuitList.py
+++ src/circuits/CircuitList.py
@@ -38,3 +38,52 @@ def measure_x_errors():
     qc.store(cr[2], expr.bit_xor(expr.lift(cr[0]), expr.lift(cr[1])))
 
     return qc
+
+def Shane_block_layout_xx__x_4():
+    """Example distillation circuit template for 2 Bell pairs."""
+    num_bell_pairs = 4
+    qr = QuantumRegister(num_bell_pairs*2, 'q')  # 4 qubits for 2 Bell pairs
+    cr = ClassicalRegister(num_bell_pairs*3, 'c')  # Classical bits for measurements + flag
+    qc = QuantumCircuit(qr, cr)
+    
+    qz = create_z()
+    qx = create_x()
+    ##ORDERING IS IMPORTANT HEREqc,[0,2,3,1],range(3)
+    qc.compose(qx, qubits=[0,2,3,1], clbits=range(3),inplace=True)
+    qc.compose(qx, qubits=[6,4,5,7], clbits=range(3,6),inplace=True)
+    qc.compose(qz, qubits=[2,3,4,5], clbits=range(6,9),inplace=True)
+
+    false1 = expr.bit_or(expr.lift(qc.clbits[2]),expr.lift(qc.clbits[5]))
+    qc.store(qc.clbits[9],false1) 
+    false_condition = expr.bit_or(expr.lift(qc.clbits[8]),expr.lift(qc.clbits[9]))
+    qc.store(qc.clbits[10],false_condition)
+    def create_x():
+        qubit_pairs = 2
+        qc = QuantumCircuit(qubit_pairs*2,3)
+        
+        qc.cx(1, 0)
+        qc.cx(2, 3)
+
+        qc.measure(0,0)
+        qc.measure(3, 1)
+        condition1 = expr.bit_xor(expr.lift(qc.clbits[0]),expr.lift(qc.clbits[1]))
+        qc.store(qc.clbits[2],condition1)   
+        return qc
+    
+    def create_z():
+        qubit_pairs = 2
+        qc = QuantumCircuit(qubit_pairs*2, 3)
+
+        qc.cx(0 ,qubit_pairs-1)
+        qc.h(0)
+        qc.cx(qubit_pairs*2-1,qubit_pairs)  
+        qc.h(qubit_pairs*2-1)     
+
+        qc.measure(0, 0)
+        qc.measure(3, 1)
+
+        condition1 = expr.bit_xor(expr.lift(qc.clbits[0]),expr.lift(qc.clbits[1]))
+        qc.store(qc.clbits[2],condition1)
+        qc.name = "Z_distillation"
+        return qc
+    return qc
