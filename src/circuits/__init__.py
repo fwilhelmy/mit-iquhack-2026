626f3450e7dd2f3f7eac002ca099044754c5c268
Félix Wilhelmy <fwilhelmy@hotmail.com>
2026-02-01 04:23:48 -0500

    Add BaseCircuit implementations for Aaron and Shane

--- src/circuits/__init__.py
+++ src/circuits/__init__.py
@@ -2,5 +2,6 @@ from circuits.BaseCircuit import BaseCircuit
 from circuits.BBPSSW import BBPSSW
 from circuits.AaronCircuit import AaronCircuit
 from circuits.FirstCircuit import FirstCircuit
+from circuits.ShaneCircuit import ShaneCircuit
 
-__all__ = ["BaseCircuit", "BBPSSW", "AaronCircuit", "FirstCircuit"]
+__all__ = ["BaseCircuit", "BBPSSW", "AaronCircuit", "FirstCircuit", "ShaneCircuit"]
